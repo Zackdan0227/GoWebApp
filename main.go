22c2c5a4a965d97ba90e72ef8befce90888f3801
Zackdan0227 <kz2028@nyu.edu>
2023-07-26 03:02:42 -0400

    added api and admin router and mounted

--- main.go
+++ main.go
@@ -1,17 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
 
-	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
-	corsMux := middlewareCors(mux)
+	r := chi.NewRouter()
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
+
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	r.Handle("/app/*", fsHandler)
+	r.Handle("/app", fsHandler)
+
+	logoHandler := http.FileServer(http.Dir(filepathRoot))
+	r.Handle("/assets/logo.png", logoHandler)
+
+	apiRouter := chi.NewRouter()
+
+	apiRouter.Get("/healthz", handlerReadiness)
+
+	adminRouter := chi.NewRouter()
+	adminRouter.Get("/metrics", apiCfg.handlerMetrics)
+
+	r.Mount("/api", apiRouter)
+	r.Mount("/admin", adminRouter)
+
+	corsMux := middlewareCors(r)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -33,4 +61,33 @@ func middlewareCors(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	htmlContent := fmt.Sprintf(`
+	<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+	
+	</html>`, cfg.fileserverHits)
+
+	fmt.Fprintln(w, htmlContent)
 }
